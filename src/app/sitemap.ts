672cb64f596c23465bcf1bb0c997b78a76657b9c
Ali-Ahmed999 <hafizaliahmed2004@gmail.com>
2025-12-19 18:41:14 +0500

    Update domain to hafizaliahmed.xyz in sitemap and robots

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,8 +1,8 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://hafiz-aliahmed-portfolio.vercel.app'
-  
+  const baseUrl = 'https://hafizaliahmed.xyz'
+
   return [
     {
       url: baseUrl,
